8b3fa1ffb6860bcb8e337244c9c1c210aa4c5ef7
ShahafErez <Shahaf30@gmail.com>
2024-08-20 17:22:46 +0300

    basic redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,16 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
 import App from "./components/App";
+import reducers from "./reducers";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
-root.render(<App />);
+const store = createStore(reducers, {}, applyMiddleware());
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
