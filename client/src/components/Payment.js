57d0cb577b5c674f802a9f957a9e64746473613e
Shahaf Erez <90973511+ShahafErez@users.noreply.github.com>
2024-08-21 19:13:35 +0300

    Billing Setup (#9)
    * stripe setups
    
    * billing setup- server and client

---
+++ client/src/components/Payment.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import { connect } from "react-redux";
+import StripeCheckout from "react-stripe-checkout";
+import * as actions from "../actions";
+
+class Payment extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        description="pay 5$ for 5 credits"
+        amount={500} // 5 dollars
+        token={(token) => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default connect(null, actions)(Payment);
