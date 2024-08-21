587f25d2d713480f372b0994df165a55b6f6db5f
ShahafErez <Shahaf30@gmail.com>
2024-08-21 17:52:19 +0300

    stripe setups

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payment from "./Payment";
 
 class Header extends Component {
   renderContent() {
@@ -16,11 +17,14 @@ class Header extends Component {
         );
 
       default:
-        return (
-          <li>
+        return [
+          <li key="1">
+            <Payment />
+          </li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
-          </li>
-        );
+          </li>,
+        ];
     }
   }
 
