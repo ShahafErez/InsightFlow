ecfc293653d280be712def401d56167bbaf5463a
ShahafErez <Shahaf30@gmail.com>
2024-08-22 18:12:25 +0300

    basic survery form

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+const SurveyField = ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
+
+export default SurveyField;
---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import SurveyField from "./SurveyField";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Subject Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" },
+];
+
+class SurveyForm extends Component {
+  renderFields() {
+    return FIELDS.map(({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+        >
+          {this.renderFields()};<button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "surveyForm",
+})(SurveyForm);
