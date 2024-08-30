08c5ee88b38d1480e8ed5948a720fa5adeb556df
ShahafErez <Shahaf30@gmail.com>
2024-08-30 12:39:02 +0300

    fixed env issue

--- client/src/components/surveys/SurveyCard.js
+++ client/src/components/surveys/SurveyCard.js
@@ -5,8 +5,8 @@ export default function SurveyCard({ survey }) {
     <div className="card" key={_id}>
       <div className="card-body">
         <h5 className="card-header ps-0">{title}</h5>
-        <h5 class="card-subtitle text-muted mt-2">{subject}</h5>
-        <p class="card-text">{body}</p>
+        <h5 className="card-subtitle text-muted mt-2">{subject}</h5>
+        <p className="card-text">{body}</p>
         <p className="right">
           Sent On: {new Date(dateSent).toLocaleDateString()}
         </p>
